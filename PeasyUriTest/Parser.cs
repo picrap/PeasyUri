e00af99e21acdd385ce663d489104aecf5ec15a4
Pascal Craponne <pascal@craponne.fr>
2022-03-10 20:03:46 +0100

    Added more tests Spoiler: one of them fails

--- PeasyUriTest/Parser.cs
+++ PeasyUriTest/Parser.cs
@@ -18,6 +18,51 @@ public class Parser
     }
 
     [Test]
+    public void RelativeUri()
+    {
+        var uriParser = new UriParser();
+        var parts = uriParser.Parse("//example.com:8042/over/there?name=ferret#nose");
+        Assert.IsNull(null, parts.Scheme);
+        Assert.AreEqual("//example.com:8042/over/there", parts.HierPart);
+        Assert.AreEqual("name=ferret", parts.Query);
+        Assert.AreEqual("nose", parts.Fragment);
+    }
+
+    [Test]
+    public void UriNoFragment()
+    {
+        var uriParser = new UriParser();
+        var parts = uriParser.Parse("foo://example.com:8042/over/there?name=ferret");
+        Assert.AreEqual("foo", parts.Scheme);
+        Assert.AreEqual("//example.com:8042/over/there", parts.HierPart);
+        Assert.AreEqual("name=ferret", parts.Query);
+        Assert.IsNull(parts.Fragment);
+    }
+
+    [Test]
+    public void UriNoQuery()
+    {
+        var uriParser = new UriParser();
+        var parts = uriParser.Parse("foo://example.com:8042/over/there#nose");
+        Assert.AreEqual("foo", parts.Scheme);
+        Assert.AreEqual("//example.com:8042/over/there", parts.HierPart);
+        Assert.IsNull(parts.Query);
+        Assert.AreEqual("nose", parts.Fragment);
+    }
+
+
+    [Test]
+    public void UriPathOnly()
+    {
+        var uriParser = new UriParser();
+        var parts = uriParser.Parse("foo://example.com:8042/over/there");
+        Assert.AreEqual("foo", parts.Scheme);
+        Assert.AreEqual("//example.com:8042/over/there", parts.HierPart);
+        Assert.IsNull(parts.Query);
+        Assert.IsNull(parts.Fragment);
+    }
+
+    [Test]
     public void Urn()
     {
         var uriParser = new UriParser();
